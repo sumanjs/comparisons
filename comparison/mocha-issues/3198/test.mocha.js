da23222b42086ab641006aa4bcb9cfe2474d8dcf
Olegzandr VD <alex@oresoftware.com>
2018-05-29 23:58:36 -0700

    pdev:set

--- comparison/mocha-issues/3198/test.mocha.js
+++ comparison/mocha-issues/3198/test.mocha.js
@@ -3,29 +3,29 @@
 var sleep = timeout => {
   return new Promise(resolve => {
     setTimeout(() => {
-    console.log(`I was sleeping ${timeout} ms`);
-    resolve();
-  }, timeout);
-});
+      console.log(`I was sleeping ${timeout} ms`);
+      resolve();
+    }, timeout);
+  });
 };
 
 var error = timeout => {
   setTimeout(() => {
     throw new Error("BOOM!!!");
-}, timeout);
+  }, timeout);
 };
 
 describe("scope", () => {
-  it ("test #1", async () => {
-  error(1000);
-  await sleep(1000);
-});
-it ("test #2", async () => await sleep(1000));
-it ("test #3", async () => {
-  error(1000);
-await sleep(1000);
-});
-it ("test #4", async () => await sleep(1000));
-it ("test #5", async () => await sleep(1000));
-it ("test #6", async () => await sleep(1000));
+  it("test #1", async () => {
+    error(1000);
+    await sleep(1000);
+  });
+  it("test #2", async () => await sleep(1000));
+  it("test #3", async () => {
+    error(1000);
+    await sleep(1000);
+  });
+  it("test #4", async () => await sleep(1000));
+  it("test #5", async () => await sleep(1000));
+  it("test #6", async () => await sleep(1000));
 });
--- comparison/mocha-issues/3198/test.suman.js
+++ comparison/mocha-issues/3198/test.suman.js
@@ -6,37 +6,37 @@ const {Test} = suman.init(module);
 let sleep = timeout => {
   return new Promise(resolve => {
     setTimeout(t => {
-    console.log(`I was sleeping ${timeout} ms`);
-  resolve();
-}, timeout);
-});
+      console.log(`I was sleeping ${timeout} ms`);
+      resolve();
+    }, timeout);
+  });
 };
 
-let error = (timeout,v) => {
+let error = (timeout, v) => {
   setTimeout(t => {
     throw new Error("BOOM!!! " + v);
-}, timeout);
+  }, timeout);
 };
 
 Test.create((describe, it) => {
 
   describe("scope", b => {
 
-  it ("test #1", async t => {
-  error(100, t.desc);
-await sleep(100);
-});
+    it("test #1", async t => {
+      error(90, t.desc);
+      await sleep(100);
+    });
 
-it ("test #2", async t => await sleep(100));
+    it("test #2", async t => await sleep(100));
 
-it ("test #3", async t => {
-  error(100, t.desc);
-  await sleep(100);
-});
+    it("test #3", async t => {
+      error(90, t.desc);
+      await sleep(100);
+    });
 
-it ("test #4", async t => await sleep(100));
-it ("test #5", async t => await sleep(100));
-it ("test #6", async t => await sleep(100));
-});
+    it("test #4", async t => await sleep(100));
+    it("test #5", async t => await sleep(100));
+    it("test #6", async t => await sleep(100));
+  });
 
 });
